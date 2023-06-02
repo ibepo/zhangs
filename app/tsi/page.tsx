84b1df1b6f4b965b84afdcaa134c523707540761
ibepo <kefanbufan@163.com>
2023-06-02 10:23:45 +0800

    Add: tsi index

---
+++ app/tsi/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className='container flex items-center justify-center  h-screen bg-gray-300'>
+      <h1>Hello, tsi page!</h1>
+    </div>
+  )
+}
